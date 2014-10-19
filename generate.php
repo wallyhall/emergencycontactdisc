740be38499aa0ee06b5f40d8b44f988cb44f74da
Matthew Hall <wally@soggysoftware.co.uk>
2014-10-19 17:13:46 +0100

    Various improvements
    First attempt at integration of share & like buttons.
    Corrected size of disc to fit in more holders.
    Added hit counter.
    Added note about data protection.

--- generate.php
+++ generate.php
@@ -7,7 +7,7 @@ if (!isset($_POST['generate'])) {
 /***/
 
 /* Code knocked up very quickly, and in a rush.
- * Ugly short-cuts taken everywhere.  You've been warned!
+ * Ugly short-cuts taken everywhere.  You have been warned!
  */
 
 define ('DOTS', '.......................');
@@ -22,6 +22,21 @@ define ('DOTS', '.......................');
 
 /***/
 
+/* Simple use counter. */
+if ($fp = fopen(dirname(__FILE__)."/hits.dat", "c+")) {
+	if (flock($fp, LOCK_EX | LOCK_NB)) {
+		$hits = intval(fgets($fp)) + 1;
+		ftruncate($fp, 0);
+		fseek($fp, 0);
+		fwrite($fp, "$hits");
+		flock($fp, LOCK_UN);
+	}
+	fclose($fp);
+}
+
+/***/
+
+/* Assumes fpdf is installed in some default include path. */
 require_once ('fpdf.php');
 
 $pdf = new fpdf('P', 'mm', 'A4');
@@ -29,100 +44,100 @@ $pdf = new fpdf('P', 'mm', 'A4');
 /***/
 
 $pdf->AddPage();
-$pdf->Image('background.png', 10, 6, -200);
+$pdf->Image('background.png', 11, 11, -220);
 
 /***/
 
-$pdf->SetFont('Arial', 'B', 22);
+$pdf->SetFont('Arial', 'B', 21);
 $pdf->SetTextColor(255, 255, 255);
-$pdf->SetXY(10, 40.5);
-$pdf->MultiCell(90, 7, "EMERGENCY\nCONTACT\nDISC", 0, 'C');
+$pdf->SetXY(11, 41.5);
+$pdf->MultiCell(81.5, 7, "EMERGENCY\nCONTACT\nDISC", 0, 'C');
 
 /***/
 
 $pdf->SetDrawColor(255, 255, 255);
 $pdf->SetFillColor(255, 255, 255);
-$pdf->Rect(24, 111, 62, 59, 'DF');
-$pdf->Rect(24, 201, 62, 59, 'DF');
+$pdf->Rect(23.5, 105, 56, 56, 'DF');
+$pdf->Rect(23.5, 187, 56, 56, 'DF');
 
 /***/
 
-$pdf->SetFont('Arial', 'BU', 11);
+$pdf->SetFont('Arial', 'BU', 10);
 $pdf->SetTextColor(255, 0, 0);
-$pdf->SetXY(10, 114);
-$pdf->Cell(90, 5, "Emergency Contact Information", 0, 1, 'C');
+$pdf->SetXY(11, 107);
+$pdf->Cell(81.5, 5, "Emergency Contact Information", 0, 1, 'C');
 
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->SetTextColor(0, 0, 0);
-$pdf->Cell(90, 6, "Name:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Name:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['nok']['name'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['nok']['name'], 0, 1, 'C');
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell(90, 6, "Relation:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Relation:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['nok']['relation'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['nok']['relation'], 0, 1, 'C');
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell(90, 6, "Phone:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Phone:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['nok']['tel'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['nok']['tel'], 0, 1, 'C');
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell(90, 6, "Address:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Address:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['nok']['address'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['nok']['address'], 0, 1, 'C');
 
 /***/
 
 $pdf->SetFont('Arial', 'BU', 12);
 $pdf->SetTextColor(255, 0, 0);
-$pdf->SetXY(10, 204);
-$pdf->Cell(90, 5, "Medical Alert Information", 0, 1, 'C');
+$pdf->SetXY(11, 189);
+$pdf->Cell(81.5, 5, "Medical Alert Information", 0, 1, 'C');
 
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->SetTextColor(0, 0, 0);
-$pdf->Cell(90, 6, "Name:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Name:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['mai']['name'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['mai']['name'], 0, 1, 'C');
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell(90, 6, "Date of Birth:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Date of Birth:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(90, 6, $_POST['mai']['dob'], 0, 1, 'C');
+$pdf->Cell(81.5, 6, $_POST['mai']['dob'], 0, 1, 'C');
 $pdf->SetFont('Arial', 'B', 10);
 
 $pdf->SetTextColor(255, 0, 0);
-$pdf->Cell(90, 6, "Medical alert:", 0, 1, 'C');
+$pdf->Cell(81.5, 6, "Medical alert:", 0, 1, 'C');
 $pdf->SetFont('Arial', '', 10);
-$pdf->MultiCell(90, 6, $_POST['mai']['alert'], 0, 'C');
+$pdf->MultiCell(81.5, 6, $_POST['mai']['alert'], 0, 'C');
 
 /***/
 
 $pdf->SetFont('Arial', 'B', 16);
 $pdf->SetTextColor(255, 0, 0);
-$pdf->SetXY(100, 220);
-$pdf->MultiCell(90, 6, "ATTENTION\nEMERGENCY SERVICES!", 0, 'C');
+$pdf->SetXY(93, 204.5);
+$pdf->MultiCell(81.5, 6, "ATTENTION\nEMERGENCY SERVICES!", 0, 'C');
 $pdf->SetTextColor(255, 255, 255);
-$pdf->SetXY(100, 233);
-$pdf->MultiCell(90, 6, "OPEN DISC\nFOR DRIVER DETAILS\nMEDICAL INFORMATION", 0, 'C');
+$pdf->SetXY(93, 217);
+$pdf->MultiCell(81.5, 6, "OPEN DISC\nFOR DRIVER DETAILS\nMEDICAL INFORMATION", 0, 'C');
 
 /***/
 
-$pdf->SetXY(100, 253);
+$pdf->SetXY(93, 235);
 $pdf->SetFont('Arial', '', 8);
-$pdf->Cell(90, 4, "Produced online at:", 0, 1, 'C');
-$pdf->SetX(100);
+$pdf->Cell(81.5, 3.5, "Produced online at:", 0, 1, 'C');
+$pdf->SetX(93);
 $pdf->SetFont('Arial', 'B', 8);
-$pdf->Cell(90, 4, "soggysoftware.co.uk/emergencycontactdisc", 0, 1, 'C');
-$pdf->SetX(100);
+$pdf->Cell(81.5, 3.5, "soggysoftware.co.uk/emergencycontactdisc", 0, 1, 'C');
+$pdf->SetX(93);
 $pdf->SetFont('Arial', '', 8);
-$pdf->Cell(90, 4, "Based on original idea and design by:", 0, 1, 'C');
-$pdf->SetX(100);
+$pdf->Cell(81.5, 3.5, "Based on original idea and design by:", 0, 1, 'C');
+$pdf->SetX(93);
 $pdf->SetFont('Arial', 'B', 8);
-$pdf->Cell(90, 4, "@MalvernCops", 0, 1, 'C');
+$pdf->Cell(81.5, 3.5, "@MalvernCops", 0, 1, 'C');
 
 /***/
 
 $pdf->SetFont('Arial', '', 10);
 $pdf->SetTextColor(50, 50, 50);
-$pdf->SetXY(105, 10);
+$pdf->SetXY(96, 10.5);
 $pdf->MultiCell(90, 5, "1. Cut along black lines for square tax disc holders, or around blue for circular holders.".
 	"\n\n2. Fold with 'EMERGENCY CONTACT DISC' on front (for window side) and 'ATTENTION' on back.".
 	"\n\n---\n".
--- index.php
+++ index.php
@@ -13,6 +13,8 @@
         <link rel="stylesheet" href="custom.css" />
 	</head>
 	<body>
+		<div id="fb-root"></div>
+	
 		<div class="container">
 			<div class="header">
 				<ul class="nav nav-pills pull-right">
@@ -24,10 +26,14 @@
 			<div class="row marketing">
 				<p class="lead">In October 2014, the UK government discontinued paper tax discs for cars.
 				<br />This left no purpose to the millions of tax disc holders owned by drivers.</p>
-				<p class="lead">@MalvernCops proposed a genius new use - the Emergency Contact Disc.
-				<br />Here you can instantly generate your own Emergency Contact Disc online.</p>
-			</div>
 
+				<p class="lead"><a href="https://twitter.com/MalvernCops">@MalvernCops</a> proposed a genius new use - the Emergency Contact Disc.
+				<br />Here you can instantly generate your own Emergency Contact Disc online.</p>
+				
+				<p>Enter contact information for your next of kin below, and any medical alert information about yourself.<br />
+				The example information can be ignored and will not appear on the final print.</p>
+			</div>
+			
 			<div class="row">
 				<div class="col-lg-6">
 					<form class="form-horizontal" role="form" method="post" action="<?php print(dirname($_SERVER['PHP_SELF'])); ?>/generate.php">
@@ -37,30 +43,30 @@
 							<legend>Emergency Contact Information (next of kin):</legend>
 							
 							<div class="form-group">
-								<label class="col-sm-2 control-label" for="nok[name]" class="sr-only">Name:</label>
+								<label class="col-sm-2 control-label" for="nok[name]">Name:</label>
 								<div class="col-sm-6">
-									<input class="form-control" type="text" name="nok[name]" />
+									<input class="form-control" type="text" id="nok[name]" name="nok[name]" placeholder="Mr Frank Furter" />
 								</div>
 							</div>
 
 							<div class="form-group">					
 								<label class="col-sm-2 control-label" for="nok[relation]">Relationship:</label>
 								<div class="col-sm-3">
-									<input class="form-control" type="text" name="nok[relation]" />
+									<input class="form-control" type="text" id="nok[relation]" name="nok[relation]" placeholder="Spouse" />
 								</div>
 							</div>
 
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="nok[tel]">Telephone:</label>
 								<div class="col-sm-3">
-									<input class="form-control" type="text" name="nok[tel]" />
+									<input class="form-control" type="text" id="nok[tel]" name="nok[tel]" />
 								</div>
 							</div>
 
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="nok[address]">Address:</label>
-								<div class="col-sm-10">
-									<input class="form-control" type="text" name="nok[address]" />
+								<div class="col-sm-6">
+									<input class="form-control" type="text" id="nok[address]" name="nok[address]" placeholder="101 The Street, IP32 7RE" />
 								</div>
 							</div>
 						</fieldset>
@@ -73,21 +79,21 @@
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="mai[name]">Name:</label>
 								<div class="col-sm-6">
-									<input class="form-control" type="text" name="mai[name]" />
+									<input class="form-control" type="text" id="mai[name]" name="mai[name]" placeholder="Mrs Peach Furter" />
 								</div>
 							</div>
 
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="mai[dob]">DOB:</label>
 								<div class="col-sm-3">
-									<input class="form-control" type="text" name="mai[dob]" />
+									<input class="form-control" type="text" id="mai[dob]" name="mai[dob]" placeholder="1964-01-03" />
 								</div>
 							</div>
 					
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="mai[alert]">Medical alert:</label>
-								<div class="col-sm-10">
-									<textarea class="form-control" name="mai[alert]"></textarea>
+								<div class="col-sm-6">
+									<textarea class="form-control" id="mai[alert]" name="mai[alert]" placeholder="N/A - No medical alerts"></textarea>
 								</div>
 							</div>
 						</fieldset>
@@ -100,9 +106,46 @@
 					</form>
 				</div>
 			</div>
+
+			<hr />
+			
+<!--			<div class="row text-center">
+				<div class="col-sm-3">
+					<script type="IN/Share" data-url="http://soggysoftware.co.uk/emergencycontactdisc/" data-counter="right"></script>
+				</div>
+				<div class="col-sm-3">
+					<div class="fb-like" data-href="http://soggysoftware.co.uk/emergencycontactdisc" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+				</div>
+				<div class="col-sm-3">
+					<div class="g-plusone" data-size="standard" data-count="true"></div>
+				</div>
+				<div class="col-sm-3">
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://soggysoftware.co.uk/emergencycontactdisc/" data-via="wallyhall">Tweet</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+				</div>
+			</div>-->
+
+			<div class="row">
+				<p class="text-muted">This service is provided unofficially, for free, and with absolutely no implied warranty by <a href="http://soggysoftware.co.uk">Matthew Hall</a>.<br />
+				Please feel free to share this website with any friends of family.</p>				
+				<p class="text-muted">Be assured that absolutely none of your information entered here is stored by us or sent to 3rd parties.<br />
+				<?php echo file_get_contents(dirname(__FILE__)."/hits.dat"); ?> discs generated.</p>
+			</div>
 		</div>
 
 	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
+		
+		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
     </body>
 </html>
