69cac192c8cb117b3dc49c958f9cb54c3177442d
Matthew Hall <wally@soggysoftware.co.uk>
2014-10-20 10:58:54 +0100

    New share/like buttons, improved instructions.
    Added share/like buttons.
    Made the instructions more readable.
    Ready for show-time?

--- index.php
+++ index.php
@@ -13,8 +13,6 @@
         <link rel="stylesheet" href="custom.css" />
 	</head>
 	<body>
-		<div id="fb-root"></div>
-	
 		<div class="container">
 			<div class="header">
 				<ul class="nav nav-pills pull-right">
@@ -24,14 +22,14 @@
 			</div>
 			
 			<div class="row marketing">
-				<p class="lead">In October 2014, the UK government discontinued paper tax discs for cars.
+				<p class="lead">In October 2014, the UK government <a href="http://www.gov.uk/dvla/nomoretaxdisc">discontinued paper tax discs</a> for cars.
 				<br />This left no purpose to the millions of tax disc holders owned by drivers.</p>
 
 				<p class="lead"><a href="https://twitter.com/MalvernCops">@MalvernCops</a> proposed a genius new use - the Emergency Contact Disc.
 				<br />Here you can instantly generate your own Emergency Contact Disc online.</p>
 				
 				<p>Enter contact information for your next of kin below, and any medical alert information about yourself.<br />
-				The example information can be ignored and will not appear on the final print.</p>
+				Any example information can be skipped and will not appear on the final print.</p>
 			</div>
 			
 			<div class="row">
@@ -45,14 +43,14 @@
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="nok[name]">Name:</label>
 								<div class="col-sm-6">
-									<input class="form-control" type="text" id="nok[name]" name="nok[name]" placeholder="Mr Frank Furter" />
+									<input class="form-control" type="text" id="nok[name]" name="nok[name]" placeholder="e.g. Mr Frank Furter" />
 								</div>
 							</div>
 
 							<div class="form-group">					
 								<label class="col-sm-2 control-label" for="nok[relation]">Relationship:</label>
 								<div class="col-sm-3">
-									<input class="form-control" type="text" id="nok[relation]" name="nok[relation]" placeholder="Spouse" />
+									<input class="form-control" type="text" id="nok[relation]" name="nok[relation]" placeholder="e.g. Spouse" />
 								</div>
 							</div>
 
@@ -66,7 +64,7 @@
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="nok[address]">Address:</label>
 								<div class="col-sm-6">
-									<input class="form-control" type="text" id="nok[address]" name="nok[address]" placeholder="101 The Street, IP32 7RE" />
+									<input class="form-control" type="text" id="nok[address]" name="nok[address]" placeholder="e.g. 101 The Street, IP32 7RE" />
 								</div>
 							</div>
 						</fieldset>
@@ -79,14 +77,14 @@
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="mai[name]">Name:</label>
 								<div class="col-sm-6">
-									<input class="form-control" type="text" id="mai[name]" name="mai[name]" placeholder="Mrs Peach Furter" />
+									<input class="form-control" type="text" id="mai[name]" name="mai[name]" placeholder="e.g. Mrs Peach Furter" />
 								</div>
 							</div>
 
 							<div class="form-group">
 								<label class="col-sm-2 control-label" for="mai[dob]">DOB:</label>
 								<div class="col-sm-3">
-									<input class="form-control" type="text" id="mai[dob]" name="mai[dob]" placeholder="1964-01-03" />
+									<input class="form-control" type="text" id="mai[dob]" name="mai[dob]" placeholder="e.g. 1964-01-03" />
 								</div>
 							</div>
 					
@@ -109,43 +107,41 @@
 
 			<hr />
 			
-<!--			<div class="row text-center">
-				<div class="col-sm-3">
-					<script type="IN/Share" data-url="http://soggysoftware.co.uk/emergencycontactdisc/" data-counter="right"></script>
-				</div>
-				<div class="col-sm-3">
-					<div class="fb-like" data-href="http://soggysoftware.co.uk/emergencycontactdisc" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
-				</div>
-				<div class="col-sm-3">
-					<div class="g-plusone" data-size="standard" data-count="true"></div>
-				</div>
-				<div class="col-sm-3">
-					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://soggysoftware.co.uk/emergencycontactdisc/" data-via="wallyhall">Tweet</a>
-						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-				</div>
-			</div>-->
+			<div class="row marketing">
+				<div id="sharrre" data-url="http://soggysoftware.co.uk/emergencycontactdisc/" data-text="DVLA declare no more tax discs! Utilise your UK tax disc holder with an Emergency Contact Disc. Create one now at http://soggysoftware.co.uk/emergencycontactdisc/" data-title="share this page"></div>
+			</div>
 
-			<div class="row">
+			<div class="row marketing">
 				<p class="text-muted">This service is provided unofficially, for free, and with absolutely no implied warranty by <a href="http://soggysoftware.co.uk">Matthew Hall</a>.<br />
 				Please feel free to share this website with any friends of family.</p>				
-				<p class="text-muted">Be assured that absolutely none of your information entered here is stored by us or sent to 3rd parties.<br />
-				<?php echo file_get_contents(dirname(__FILE__)."/hits.dat"); ?> discs generated.</p>
+				<p class="text-muted">Be assured that absolutely none of your information entered here is stored by us or sent to 3<sup>rd</sup> parties.<br />
+				<?php echo file_get_contents(dirname(__FILE__)."/hits.dat"); ?> discs generated since 2014-10-18.</p>
 			</div>
 		</div>
 
 	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-		<script>(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
-		
-		<script src="https://apis.google.com/js/platform.js" async defer></script>
-		
-		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+        
+        <script src="<?php print dirname($_SERVER['PHP_SELF']); ?>/externals/Sharrre/jquery.sharrre.js"></script>
+        <script type="text/javascript">
+        $('#sharrre').sharrre({
+		  share: {
+			googlePlus: true,
+			facebook: true,
+			twitter: true,
+			linkedin: true,
+		  },
+		  buttons: {
+			googlePlus: {size: 'tall', annotation:'bubble'},
+			facebook: {layout: 'box_count'},
+			twitter: {count: 'vertical'},
+			digg: {type: 'DiggMedium'},
+			linkedin: {counter: 'top'},
+		  },
+		  enableHover: false,
+		  enableCounter: false,
+		  enableTracking: true
+		});
+        </script>
     </body>
 </html>
